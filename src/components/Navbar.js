25bf6180b7fbd9269c0b6facb8a463f4991bea73
mabilog <60443712+angelo-mab@users.noreply.github.com>
2021-06-09 14:53:09 -0400

    im leaving

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <nav className="navbar">
       <ul className="nav-links">
-        <Link to="/" ><li>Home</li></Link>
+        <Link to="/"><li>Home</li></Link>
         <Link to="/about"><li>About</li></Link>
         <Link to="/profile"><li>Profile</li></Link>
         <Link to="/shop"><li>Shop</li></Link>
