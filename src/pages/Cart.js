a447f8a0ba252261a7d7a52cd50db89d49dcff1f
mabilog <60443712+angelo-mab@users.noreply.github.com>
2021-06-14 15:08:34 -0400

    this is going to be fun

---
+++ src/pages/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <div>
+      <h2>Cart</h2>
+      <p>Modal project</p>
+    </div>
+  )
+}
+
+export default Cart
---
+++ src/pages/Shop.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+      <h2>This shop includes:</h2>
+      <ul>
+        <li>Accessories
+          <ul>
+            <li>Hats</li>
+            <li>Rings</li>
+            <li>Belts</li>
+          </ul>
+        </li>
+        <li>Tops
+          <ul>
+            <li>Sweaters</li>
+            <li>Long Sleaves</li>
+            <li>Graphic Ts</li>
+          </ul>
+        </li>
+        <li>Bottoms
+          <ul>
+            <li>Pants</li>
+            <li>Shorts</li>
+          </ul>
+        </li>
+        <li>Shoes
+          <ul>
+            <li>High tops</li>
+            <li>Low tops</li>
+            <li>Sneakers</li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Shop
