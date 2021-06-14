caf3a7d2afc2d6975d4561585b77728e0e0105ab
mabilog <60443712+angelo-mab@users.noreply.github.com>
2021-06-14 15:37:13 -0400

    lets dot his at home!

---
+++ src/components/Navbar.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Navbar</h1>
+      <ul>
+        <li>Shop
+          <ul>
+          <li>Accessories
+          <ul>
+            <li>Hats</li>
+            <li>Rings</li>
+            <li>Belts</li>
+          </ul>
+        </li>
+        <li>Tops
+          <ul>
+            <li>Sweaters</li>
+            <li>Long Sleaves</li>
+            <li>Graphic Ts</li>
+          </ul>
+        </li>
+        <li>Bottoms
+          <ul>
+            <li>Pants</li>
+            <li>Shorts</li>
+          </ul>
+        </li>
+        <li>Shoes
+          <ul>
+            <li>High tops</li>
+            <li>Low tops</li>
+            <li>Sneakers</li>
+          </ul>
+        </li>
+          </ul>
+        </li>
+        <li>Search Bar</li>
+        <li>Log in page</li>
+        <li>Cart</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
