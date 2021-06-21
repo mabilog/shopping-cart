6f454d44629947db92f472daa77a625300309cb9
mabilog <60443712+angelo-mab@users.noreply.github.com>
2021-06-21 15:28:25 -0400

    i didnt do anything

--- src/components/Food.js
+++ src/components/Food.js
@@ -6,19 +6,19 @@ const Food = [
     id: 'burger',
     name: 'Cheeseburger',
     price: 10,
-    img: 'https://images.unsplash.com/photo-1586190848861-99aa4a171e90?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80',
+    img: 'E94j3rMcxlw',
   },
   {
     id: 'fries',
     name: 'Fries',
     price: 5,
-    img: 'https://images.unsplash.com/photo-1576100246753-1c1b4fa73481?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80',
+    img: '5I5aqYJrdso',
   },
   {
     id: 'milkshake',
     name: 'Milkshake',
     price: 8,
-    img: 'https://images.unsplash.com/photo-1586917049334-0f99406d8a6e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1233&q=80',
+    img: 'uy9DJw9e_vs',
   },
 ]
 
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Link, useHistory } from 'react-router-dom';
+import { BsChevronLeft } from 'react-icons/bs';
+import styled from 'styled-components';
 
-const Item = () => {
+
+const Item = ({item, addCartItem}) => {
+  const [showCompleteOrder, setShowCompleteOrder] = useState(false);
+  const { goBack } = useHistory();
+
+  const handleAdd = () => {
+    setShowCompleteOrder(true);
+    addCartItem(item);
+  }
   return (
-    <div>
-      
-    </div>
+    <ItemWrapper>
+      <img
+      src={`https://source.unsplash.com/${item.img}/640x640`}
+      alt={item.id}
+      />
+      <InfoWrapper>
+        <h1>{item.name}</h1>
+        <p>{item.price.toFixed(2)} USD</p>
+        <button onClick={handleAdd}>Add to cart</button>
+        {showCompleteOrder && (
+          <Link to='/cart'>
+            <button>Complete Order</button>
+          </Link>
+        )}
+        <button onClick={goBack}>
+          <BsChevronLeft/> Go Back
+        </button>
+      </InfoWrapper>
+    </ItemWrapper>
   )
 }
 
+const ItemWrapper = styled.div``;
+
+const InfoWrapper = styled.div``;
 export default Item
